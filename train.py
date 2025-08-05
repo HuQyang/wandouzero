016109b6e156588736bb9d9cdae0ed8d599f7a7b
HuQyang <whiary1201@gmail.com>
2025-08-05 15:27:44 +0200

    file

--- train.py
+++ train.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     flags.num_actors = 12
     flags.num_threads = 4
     flags.load_model = False
-    flags.batch_size = 64
+    flags.batch_size = 32
     flags.sleep_time = 20
     flags.savedir = "mini_oracle_batch16"
     flags.use_wandb = False
     flags.save_interval = 20
-    flags.unroll_length = 60
+    flags.unroll_length = 100
     # os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     train(flags)
